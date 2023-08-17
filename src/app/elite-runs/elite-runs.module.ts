76f3aad1710a955d12353b885ac3ba2286457157
ljcla <ljclaeyssen@gmail.com>
2023-08-17 22:53:03 +0200

    feat: more assets

--- src/app/elite-runs/elite-runs.module.ts
+++ src/app/elite-runs/elite-runs.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EliteRunsRoutingModule } from './elite-runs-routing.module';
+import { EliteRunsComponent } from './elite-runs.component';
+import { SingleRunComponent } from './single-run/single-run.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    EliteRunsComponent,
+    SingleRunComponent
+  ],
   imports: [
     CommonModule,
     EliteRunsRoutingModule
